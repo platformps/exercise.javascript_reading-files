0e6cf82047cb891a1ee10e69cffd37f459d613d6
David Frary <fraryd@gmail.com>
2020-07-04 00:15:45 -0400

    finished edits on 'read-files.js'

--- assets/js/read-files.js
+++ assets/js/read-files.js
@@ -1,7 +1,9 @@
-function printSingleFileAsText(evt) {
-    let fileContent = getSingleFileAsText(evt);
-    console.log(fileContent);
-}
+
+
+// function printSingleFileAsText(evt) {
+//     let fileContent = getSingleFileAsText(evt);
+//     console.log(fileContent);
+// }
 
 function getSingleFileAsText(evt) {
     //Retrieve the first (and only!) File from the FileList object
@@ -11,21 +13,22 @@ function getSingleFileAsText(evt) {
         let fileReader = new FileReader();
         fileReader.onload = function(event) {
             let contents = event.target.result;
-            let words = getWords(contents);
+            // let words = getWords(contents);
+            console.log(contents);
         }
         return fileReader.readAsText(file)
     }
 }
 
-function getWords(s) {
-    let result = "";
-    let i = 0;
-    while (i < s.length) {
-        result += s[i];
-        i++;
-    }
-    return result;
-}
+// function getWords(s) {
+//     let result = "";
+//     let i = 0;
+//     while (i < s.length) {
+//         result += s[i];
+//         i++;
+//     }
+//     return result;
+// }
 
 
 let fileInput = document.getElementById('fileinput');
