6e7ed30c4ae36ce31b8f0b3135ba108cb39ff8a9
Laura Godinez <issagodinez@Lauras-MacBook-Pro.local>
2020-07-05 10:37:33 -0400

    Finished Edits to Reading Exercise

--- assets/js/read-files.js
+++ assets/js/read-files.js
@@ -1,32 +1,34 @@
 function printSingleFileAsText(evt) {
-    let fileContent = getSingleFileAsText(evt);
-    console.log(fileContent);
+  let fileContent = getSingleFileAsText(evt);
+  console.log(fileContent);
 }
 
 function getSingleFileAsText(evt) {
-    //Retrieve the first (and only!) File from the FileList object
-    let result = "";
-    let file = evt.target.files[0];
-    if (file) {
-        let fileReader = new FileReader();
-        fileReader.onload = function(event) {
-            let contents = event.target.result;
-            let words = getWords(contents);
-        }
-        return fileReader.readAsText(file)
-    }
+  //Retrieve the first (and only!) File from the FileList object
+  let result = "";
+  let file = evt.target.files[0];
+  if (file) {
+    let fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      let contents = event.target.result;
+      let words = getWords(contents);
+      console.log(contents);
+    };
+    return fileReader.readAsText(file);
+  }
 }
 
 function getWords(s) {
-    let result = "";
-    let i = 0;
-    while (i < s.length) {
-        result += s[i];
-        i++;
-    }
-    return result;
+  let result = "";
+  let i = 0;
+  while (i < s.length) {
+    result += s[i];
+    i++;
+  }
+  return result;
 }
 
-
-let fileInput = document.getElementById('fileinput');
-fileInput.addEventListener('change', getSingleFileAsText, false);
+window.onload = function(event) {
+  let fileInput = document.getElementById("fileinput");
+  fileInput.addEventListener("change", getSingleFileAsText, false);
+};
