73507fcf823d3a417fbb13b250bb6d25c871fd68
AnjaliG927 <anjali.ghaligi@gmail.com>
2020-07-05 10:13:22 -0400

    completed

--- assets/js/read-files.js
+++ assets/js/read-files.js
@@ -1,3 +1,7 @@
+function printSingleFileAsText(evt) {
+    let fileContent = getSingleFileAsText(evt);
+    console.log(fileContent);
+}
 
 function getSingleFileAsText(evt) {
     //Retrieve the first (and only!) File from the FileList object
@@ -7,11 +11,23 @@ function getSingleFileAsText(evt) {
         let fileReader = new FileReader();
         fileReader.onload = function (event) {
             let contents = event.target.result;
-            console.log(contents);
+            let words = getWords(contents);
+            console.log(words);
         }
         return fileReader.readAsText(file)
     }
 }
+
+function getWords(s) {
+    let result = "";
+    let i = 0;
+    while (i < s.length) {
+        result += s[i];
+        i++;
+    }
+    return result;
+}
+
 
 let fileInput = document.getElementById('fileinput');
 fileInput.addEventListener('change', getSingleFileAsText, false);
